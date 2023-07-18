cbcd61b9f9b8b19985b8c30074d945f77e87d596
Avinash Kumar Mishra <avinash.kumar.mishra@bluevector.ai>
2023-07-18 23:50:59 +0530

    duplicate files deleter

---
+++ delete_duplicate_files.py
@@ -0,0 +1,19 @@
+import os , hashlib
+from pathlib import Path
+
+path = input("Please enter the path to find duplicates : ")
+
+file_list = os.walk(path)
+
+unique = dict()
+for root,folders,files in file_list:
+    for file in files:
+        path = Path(os.path.join(root,file))
+        fileHash = hashlib.md5(open(path,'rb').read()).hexdigest()
+        print((fileHash))
+        if fileHash not in unique:
+            unique[fileHash] = path
+            
+        else:
+            os.remove(path)
+            print(f"{path} has been deleted")
